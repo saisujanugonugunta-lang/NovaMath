a87aff9a349dda5dca15c29e0be677f391a404be
saisujanugonugunta-lang <saisujanugonugunta@gmail.com>
2026-09-10 21:34:00 +0530

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,17 @@
-import { defineConfig } from 'eslint/config';
-import nextVitals from 'eslint-config-next/core-web-vitals';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
-export default defineConfig([
-  ...nextVitals,
-  { ignores: ['.next/**', 'node_modules/**'] }
-]);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  { ignores: ['.next/**', 'node_modules/**', 'out/**'] },
+];
+
+export default eslintConfig;
